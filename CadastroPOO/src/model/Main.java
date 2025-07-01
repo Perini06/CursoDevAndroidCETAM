5b9f79d7f93138b15374f90078d1c3149e5e8c0f
Ryan Perini <periniryan06@gmail.com>
2025-07-01 15:54:41 -0400

    Primeiro commit

--- CadastroPOO/src/model/Main.java
+++ CadastroPOO/src/model/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		Aluno aluno1 = new Aluno("Ryan", 20, "Ciência da Computação");
         aluno1.exibirDados();
+        
+        Aluno aluno2 = new Aluno("Teste", 19, "Teste");
+        aluno2.exibirDados();
 	}
 
 }
