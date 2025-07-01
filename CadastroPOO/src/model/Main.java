1bb2e946dc2c83fdf33481d2d897acc6361fd477
Ryan Perini <periniryan06@gmail.com>
2025-07-01 15:47:28 -0400

    Primeiro commit - Classe Aluno e teste Main implementados

---
+++ CadastroPOO/src/model/Main.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Aluno aluno1 = new Aluno("Ryan", 20, "Ciência da Computação");
+        aluno1.exibirDados();
+	}
+
+}
